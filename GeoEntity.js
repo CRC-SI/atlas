a127ad9c884373960e338344ffefdc8500c40944
Brendan Studds <brendan.studds@gmail.com>
2013-09-25 22:20:57 +1000

    Implemented GeoEntity.js abstract class. Added DeveloperError exception class (from cesium.)

--- GeoEntity.js
+++ GeoEntity.js
@@ -1,1 +1,36 @@
+define([
+], function () {
+  var geoEntity = {
+    _centroid: null,
+    _area: null,
+    _visible: null,
+  };
 
+  geoEntity.prototype.getCentroid = function() {
+    throw new DeveloperError('Can not call method of abstract GeoEntity');
+  };
+
+  geoEntity.prototype.getArea = function() {
+    throw new DeveloperError('Can not call method of abstract GeoEntity');
+  };
+
+  geoEntity.prototype.isVisible = function() {
+    return this._visible;
+  };
+
+  geoEntity.prototype.show = function() {
+    throw new DeveloperError('Can not call method of abstract GeoEntity');
+  };
+
+  geoEntity.prototype.hide = function() {
+    throw new DeveloperError('Can not call method of abstract GeoEntity');
+  };
+
+  geoEntity.prototype.toggleVisibility = function() {
+    if (this._visible) {
+      this.hide();
+    } else {
+      this.show();
+    }
+  };
+});
