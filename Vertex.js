7627534933f8baa20fb498ac6febadfbc95cae3b
Brendan Studds <brendan.studds@gmail.com>
2013-09-30 16:19:46 +1000

    Fixed JSDoc.

--- Vertex.js
+++ Vertex.js
@@ -2,10 +2,13 @@ define([
 ], function () {
 
   /**
-   * Constructor for a new Vertex.
+   * Constructor for a new Vertex object.
    * @param {number} [u=0] - the 'x' coordinate.
    * @param {number} [v=0] - the 'y' coordinate.
    * @param {number} [w=0] - the 'z' coordinate.
+   *
+   * @alias Vertex
+   * @constructor
    */
   var Vertex = function (u, v, w) {
     this.x = (u || 0);
