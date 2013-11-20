ebafa95a7407e565890e0176b08f6ced75cf1465
Brendan Studds <brendan.studds@gmail.com>
2013-11-20 12:41:21 +1100

    Formatting.

--- model/DeveloperError.js
+++ model/DeveloperError.js
@@ -1,58 +1,59 @@
 /*global define*/
-define([], function() {
-    "use strict";
+define([
+], function() {
+  "use strict";
+
+  /**
+   * Constructs an exception object that is thrown due to a developer error, e.g., invalid argument,
+   * argument out of range, etc.  This exception should only be thrown during development;
+   * it usually indicates a bug in the calling code.  This exception should never be
+   * caught; instead the calling code should strive not to generate it.
+   * <br /><br />
+   * On the other hand, a {@link RuntimeError} indicates an exception that may
+   * be thrown at runtime, e.g., out of memory, that the calling code should be prepared
+   * to catch.
+   *
+   * @alias DeveloperError
+   *
+   * @param {String} [message=undefined] The error message for this exception.
+   *
+   * @constructor
+   */
+  var DeveloperError = function(message) {
+    /**
+     * 'DeveloperError' indicating that this exception was thrown due to a developer error.
+     * @type {String}
+     * @constant
+     */
+    this.name = 'DeveloperError';
 
     /**
-     * Constructs an exception object that is thrown due to a developer error, e.g., invalid argument,
-     * argument out of range, etc.  This exception should only be thrown during development;
-     * it usually indicates a bug in the calling code.  This exception should never be
-     * caught; instead the calling code should strive not to generate it.
-     * <br /><br />
-     * On the other hand, a {@link RuntimeError} indicates an exception that may
-     * be thrown at runtime, e.g., out of memory, that the calling code should be prepared
-     * to catch.
-     *
-     * @alias DeveloperError
-     *
-     * @param {String} [message=undefined] The error message for this exception.
-     *
-     * @constructor
+     * The explanation for why this exception was thrown.
+     * @type {String}
+     * @constant
      */
-    var DeveloperError = function(message) {
-        /**
-         * 'DeveloperError' indicating that this exception was thrown due to a developer error.
-         * @type {String}
-         * @constant
-         */
-        this.name = 'DeveloperError';
+    this.message = message;
 
-        /**
-         * The explanation for why this exception was thrown.
-         * @type {String}
-         * @constant
-         */
-        this.message = message;
+    var e = new Error();
 
-        var e = new Error();
+    /**
+     * The stack trace of this exception, if available.
+     * @type {String}
+     * @constant
+     */
+    this.stack = e.stack;
+  };
 
-        /**
-         * The stack trace of this exception, if available.
-         * @type {String}
-         * @constant
-         */
-        this.stack = e.stack;
-    };
+  DeveloperError.prototype.toString = function() {
+    var str = this.name + ': ' + this.message;
 
-    DeveloperError.prototype.toString = function() {
-        var str = this.name + ': ' + this.message;
+    // TODO(bpstudds): Is this good practice?
+    if (typeof this.stack !== undefined) {
+        str += '\n' + this.stack.toString();
+    }
 
-        // TODO(bpstudds): Is this good practice?
-        if (typeof this.stack !== undefined) {
-            str += '\n' + this.stack.toString();
-        }
+    return str;
+  };
 
-        return str;
-    };
-
-    return DeveloperError;
+  return DeveloperError;
 });
