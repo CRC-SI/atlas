f56bec8db80e1169179a282557bad74a0015bd24
Brendan Studds <brendan.studds@gmail.com>
2013-11-26 16:40:39 +1100

    model/GeoEntity: added jsdoc, update constructor to accept id and parent.

--- model/GeoEntity.js
+++ model/GeoEntity.js
@@ -1,25 +1,39 @@
 define([
-], function () {
-  
+  'atlas/lib/extends',
+  'atlas/events/EventTarget'
+], function (extend, EventTarget) {
+
   /**
-   * Defines the interface for atlas/GeoEntity. GeoEntity is a purely
-   * abstract module that is extended by other atlas entities.
+   * A GeoEntity is an abstract class that represents an entity that
+   * has a defined place in 3d space. A GeoEntity is a purely
+   * abstract module that is extended by other atlas entities that specify
+   * what is this particular GeoEntity represents (eg. a polygon or a line).
+   *
+   * @param {Object} [id]     The ID of this GeoEntity.
+   * @param {Object} [parent] The parent of the GeoEntity.
    * 
-   * @see{Object}
+   * @see{Feature}
    * @see{Polygon}
    * @see{Network}
    * @see{Line}
    * @see{Vertex}
    * @see{PointHandle}
    *
-   * @alias GeoEntity
+   * @abstract
+   * @extends {EventTarget}
+   * @alias atlas/model/GeoEntity
    * @constructor
    */
-  var GeoEntity = function () {
+  var GeoEntity = function (id, parent) {
+    GeoEntity.base.constructor.call(this, parent);
+
+    this.id = id;
     this.centroid = null;
     this.area = 0;
     this.visible = false;
   };
+  // Inherit from EventTarget
+  extend(EventTarget, GeoEntity);
 
   /**
    * Get the footprint centroid of the GeoEntity.
@@ -48,7 +62,7 @@ define([
   /**
    * Show this GeoEntity.
    * Delegated to the RenderManager
-   * @see {RenderManager}.
+   * @see {RenderManager}
    */
   GeoEntity.prototype.show = function() {
     throw new DeveloperError('Can not call method of abstract GeoEntity');
@@ -57,7 +71,7 @@ define([
   /**
    * Hide this GeoEntity.
    * Delegated to the RenderManager
-   * @see {RenderManager}.
+   * @see {RenderManager}
    */
   GeoEntity.prototype.hide = function() {
     throw new DeveloperError('Can not call method of abstract GeoEntity');
@@ -66,7 +80,7 @@ define([
   /**
    * Remove this GeoEntity from the scene (vs. hiding it).
    * Delegated to the RenderManager
-   * @see {RenderManager}.
+   * @see {RenderManager}
    */
   GeoEntity.prototype.remove = function() {
     throw new DeveloperError('Can not call method of abstract GeoEntity');
