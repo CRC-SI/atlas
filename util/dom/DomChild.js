eb5b538047ba94bcc4d597a7498b91ad88a31fd0
Brendan Studds <brendan.studds@gmail.com>
2013-11-26 16:26:13 +1100

    Added 2 classes to DOM element children and classes.

---
+++ util/dom/DomChild.js
@@ -0,0 +1,66 @@
+/*
+ * A utility class for managing the children of DOM Elements.
+ */
+define([
+], function () {
+
+  /**
+   * A utility class containing static functions for managing the child
+   * elements on a DOM node.
+   * @author Brendan Studds
+   * @exports atlas/util/dom/DomChild
+   */
+  var DomChild = {
+
+    /**
+     * Gets all of the child elements of the given DOM node.
+     * @param  {HTMLElement} element The DOM node.
+     * @return {HTMLElement[]}       A list of all child nodes.
+     */
+    getChildren: function (element) {
+      if (!element.hasChildNodes()) {
+        return [];
+      }
+      var children = [];
+      children.append(element.firstElementChild);
+      var sibling = children[0];
+      while ((sibling = sibling.nextElementSibling) !== null) {
+        children.push(sibling);
+      }
+      return children;
+    },
+
+    /**
+     * Removes all children from the given DOM node.
+     * @param {HTMLElement} element The DOM node.
+     */
+    removeChildren: function (element) {
+      var children = DomChild.getChildren(element);
+      for (var child in children) {
+        element.removeChild(child);
+      }
+    },
+
+    /**
+     * Adds a given HTMLElement as a child to to given DOM node.
+     * @param {HTMLElement} element The DOM node to add to.
+     * @param {HTMLElement} child   The DOM node to add.
+     */
+    addChild: function (element, child) {
+      element.appendChild(child);
+    },
+
+    /**
+     * Adds a list of HTMLElements to the given DOM node.
+     * @param {HTMLElement} element    The DOM node to add to.
+     * @param {HTMLElement[]} children The list of nodes to add.
+     */
+    addChildren: function (element, children) {
+      for (var child in children) {
+        element.appendChild(child);
+      }
+    }
+  };
+
+  return DomChild;
+});
---
+++ util/dom/DomClass.js
@@ -0,0 +1,44 @@
+/**
+ * A utility class for adding and removing classes to DOM HTMLElements.
+ */
+define([
+], function () {
+
+  /**
+   * A utility class containing static functions for managing a DOM 
+   * elements classes.
+   * @author Brendan Studds
+   * @exports atlas/util/dom/DomClass
+   */
+  var DomClass = {
+
+    /**
+     * Adds the given class to the given element.
+     * @param {HTMLElement} element The DOM HtmlElement to change.
+     * @param {String} classStr     The class to add to the DOM element
+     */
+    add: function(element, classStr) {
+      element.class.add(classStr);      
+    },
+
+    /**
+     * Removes the given class to the given element.
+     * @param {HTMLElement} element The DOM HtmlElement to change.
+     * @param {String} classStr     The class to remove
+     */
+    remove: function(element, classStr) {
+      element.class.remove(classStr);      
+    },
+
+    /**
+     * Toggles the given class to the given element.
+     * @param {HTMLElement} element The DOM HtmlElement to change.
+     * @param {String} classStr     The class to toggle
+     */
+    toggle: function(element, classStr) {
+      element.class.toggle(classStr);      
+    },
+  };
+
+  return DomClass;
+});
