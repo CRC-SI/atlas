392ce69a17d4eb9c6598a33ab95fdd6c2440b05a
Brendan Studds <brendan.studds@gmail.com>
2013-12-02 17:27:17 +1100

    model/Style: added default.

--- model/Style.js
+++ model/Style.js
@@ -1,6 +1,8 @@
 // Style.js
 define([
-], function () {
+  './Colour'
+], function (Colour) {
+  "use strict";
 
   /**
    * A Style object defines the colour and opacity of a polygon's
@@ -9,37 +11,39 @@ define([
    * @alias atlas/model/Style
    * @constructor
    */
-  var Style = function () {
+  var Style = function (fillColour, borderColour, borderWidth) {
     /**
      * Fill colour of this Style.
      * @type {Colour}
      */
-    this.fillColour = null;
+    this.fillColour = (fillColour || Colour.GREEN);
 
     /**
      * Fill opacity of this Style.
      * @type {Number}
      */
-    this.opacity = 1.0;
+    // this.opacity = 1.0;
 
     /**
      * Border colour of this Style.
      * @type {Colour}
      */
-    this.borderColour = {};
+    this.borderColour = (fillColour || Colour.GREEN);
 
     /**
      * Border opacity of this Style.
      * @type {Number}
      */
-    this.borderOpacity = 1.0;
+    // this.borderOpacity = 1.0;
 
     /**
      * Border width of this Style.
      * @type {Number}
      */
-    this.borderWidth = 1.0;
+    this.borderWidth = (borderWidth || 1.0);
   };
+
+  Style.DEFAULT = new Style(Colour.GREEN, Colour.GREEN, 1);
 
   return Style;
 });
