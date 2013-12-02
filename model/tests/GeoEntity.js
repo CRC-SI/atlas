041ada47e332b13794b02c787d9a67aa4a03d895
Brendan Studds <brendan.studds@gmail.com>
2013-12-02 11:45:59 +1100

    model/tests/GeoEntity: added EventTarget test case.

--- model/tests/GeoEntity.js
+++ model/tests/GeoEntity.js
@@ -14,6 +14,7 @@ define([
       // summary:
       //      Create an GeoEntity object to test presence of functions.
       geoEntity = new GeoEntity(); 
+      console.error("setup", geoEntity.dispatchEvent);
     },
 
     tearDown: function() {
@@ -29,6 +30,21 @@ define([
       doh.assertEqual(0, geoEntity.area);
       doh.assertEqual(false, geoEntity.visible);
     }
+
+    /*
+    testEventTargetness: function() {
+      // Members
+      doh.assertNotEqual('undefined', typeof(geoEntity.eventHandlers));
+      doh.assertNotEqual('undefined', typeof(geoEntity.nextEventListenerId));
+      doh.assertNotEqual('undefined', typeof(geoEntity.parent));
+
+      // Functions
+      doh.assertEqual('function', typeof(geoEntity.dispatchEvent()));
+      doh.assertEqual('function', typeof(geoEntity.addEventListener()));
+      doh.assertEqual('function', typeof(geoEntity.removeEventListener()));
+      doh.assertEqual('function', typeof(geoEntity.handleEvent()));
+    }
+    */
   }).register(doh);
 
 });
