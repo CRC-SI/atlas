01edd90532856dfaa00d9ee832e6da1c32219e46
Brendan Studds <brendan.studds@gmail.com>
2013-12-04 17:27:25 +1100

    Atlas: added publish and subscribe API. Changed how managers are tracked.

--- Atlas.js
+++ Atlas.js
@@ -9,26 +9,35 @@ define([
    * @author  Brendan Studds
    * @version 1.0
    *
+   * @abstract
    * @alias atlas/Atlas
    * @constructor
    */
   var Atlas = function () {
     // In Atlas assume managers are defined. Implementations will include these.
     this.managers = {};
-    this.managers['render'] = [];
-    this.managers['dom'] = [];
+    this.managers['render'] = {};
+    this.managers['dom'] = {};
+    this.managers['events'] = {};
   };
 
 
-  Atlas.prototype.setManager = function (name, manager) {
-    this.managers[name].unshift(manager);
+  Atlas.prototype.setManager = function (type, manager) {
+    var oldManager = this.managers[type];
+    this.managers[type] = manager;
+    return oldManager;
   };
 
-  Atlas.prototype.removeManager = function (name, manager) {
-    if (this.managers[name] !== undefined) {
-      delete this.managers[name];
-    }
-  };
+  // If you only have one manager you can't remove it.
+  // TODO(bpstudds): Look into having multiple managers and switching between them?
+  // Atlas.prototype.removeManager = function (type, manager) {
+  //   if (len(this.managers[type]) == 1) {
+  //     throw new DeveloperError('Can not remove last manager for type ' + type);
+  //   }
+  //   if (this.managers[type][-1] !== undefined) {
+  //     delete this.managers[type][-1];
+  //   }
+  // };
 
   /**
    * Function to initialise the Atlas instance.
@@ -53,8 +62,27 @@ define([
     this.managers.dom.show();
   };
 
+  /**
+   * Allows the Host application to publish an event to the internal
+   * Atlas event system.
+   * @param  {String} eventName - The type of the event to be published.
+   * @param  {Object} [args] - Arguments relevant to the event.
+   */
+  Atlas.prototype.publish = function (eventName, args) {
+    this.managers.events.handleExternEvent(eventName, args);
+  };
 
-  Atlas.prototype.addPolygon = function (params) {
+  /**
+   * Allows the Host application to subscribe to internal events of the Atlas
+   * event system.
+   * @param  {String}   eventName - The event tyep to subscribe to.
+   * @param  {Function} callback - The callback that will be called when the event occurs.
+   */
+  Atlas.prototype.subscribe = function (eventName, callback) {
+    this.managers.events.addEventHandler('intern', eventName, callback);
+  };
+
+  Atlas.prototype.addPolygon = function (args) {
     throw new DeveloperError('Can not call abstract method on Atlas.');
   };
 
