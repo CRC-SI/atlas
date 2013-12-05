66a6d82d1d1426fb4ced1d194b4f3e290d15d018
Brendan Studds <brendan.studds@gmail.com>
2013-12-05 16:12:58 +1100

    Atlas: modified how manager refernces work. Added function to add and render a Feature (may be removed).

--- Atlas.js
+++ Atlas.js
@@ -14,28 +14,42 @@ define([
    * @constructor
    */
   var Atlas = function () {
-    // In Atlas assume managers are defined. Implementations will include these.
-    this.managers = {};
-    this.managers['render'] = {};
-    this.managers['dom'] = {};
-    this.managers['events'] = {};
+    /**
+     * A mapping of every manager type in Atlas to the manager instance. This
+     * object is created on Atlas, but the manager instances are set by each
+     * manager upon creation.
+     * @type {Object}
+     */
+    this._managers = {};
+    this._managers['render'] = {};
+    this._managers['dom'] = {};
+    this._managers['event'] = {};
   };
 
-
+  /**
+   * Allows the a particular manager to be replaced with another.
+   * @param {String} type - The type of manager to replaced, ie 'dom' or 'render'
+   * @param {Object} manager - The new manager.
+   * @returns {Object} The old manager.
+   */
   Atlas.prototype.setManager = function (type, manager) {
-    var oldManager = this.managers[type];
-    this.managers[type] = manager;
-    return oldManager;
+    if (!(type in this._managers)) {
+      throw new DeveloperError('Attempted to set manager of unknown type.');
+    } else {
+      var oldManager = this._managers[type];
+      this._managers[type] = manager;
+      return oldManager;
+    }
   };
 
   // If you only have one manager you can't remove it.
   // TODO(bpstudds): Look into having multiple managers and switching between them?
   // Atlas.prototype.removeManager = function (type, manager) {
-  //   if (len(this.managers[type]) == 1) {
+  //   if (len(this._managers[type]) == 1) {
   //     throw new DeveloperError('Can not remove last manager for type ' + type);
   //   }
-  //   if (this.managers[type][-1] !== undefined) {
-  //     delete this.managers[type][-1];
+  //   if (this._managers[type][-1] !== undefined) {
+  //     delete this._managers[type][-1];
   //   }
   // };
 
@@ -44,22 +58,23 @@ define([
    * @param {HTMLElement} element The initial DOM element to render in.
    */
   Atlas.prototype.initialise = function (element) {
-    this.managers.dom.setDomEle(element);
-    this.managers.dom.populateDomEle(element);
+    console.log(this._managers);
+    this._managers.dom.setDom(element);
+    this._managers.dom.populateDom(element);
   };
 
   /**
    * Function to show the Atlas render.
    */
   Atlas.prototype.show = function () {
-    this.managers.dom.show();
+    this._managers.dom.show();
   };
 
   /**
    * Function to hide the Atlas render.
    */
   Atlas.prototype.hide = function () {
-    this.managers.dom.show();
+    this._managers.dom.show();
   };
 
   /**
@@ -69,7 +84,7 @@ define([
    * @param  {Object} [args] - Arguments relevant to the event.
    */
   Atlas.prototype.publish = function (eventName, args) {
-    this.managers.events.handleExternEvent(eventName, args);
+    this._managers.event.handleExternEvent(eventName, args);
   };
 
   /**
@@ -79,7 +94,21 @@ define([
    * @param  {Function} callback - The callback that will be called when the event occurs.
    */
   Atlas.prototype.subscribe = function (eventName, callback) {
-    this.managers.events.addEventHandler('intern', eventName, callback);
+    this._managers.event.addEventHandler('intern', eventName, callback);
+  };
+
+  Atlas.prototype.addFeature = function (id, args) {
+    if (typeof id === 'undefined') {
+      throw new DeveloperError('Can add Feature without specifying id');
+    } else {
+      // Add EventManger to the args for the feature.
+      args.eventManager = this._managers.event;
+      // Add the RenderManager to the args for the feature.
+      args.renderManager = this._managers.render;
+      var feature = new this._managers.render.FeatureClass(id, args);
+      this.addEntity(feature);
+      return feature;
+    }
   };
 
   Atlas.prototype.addPolygon = function (args) {
@@ -88,11 +117,11 @@ define([
 
 
   Atlas.prototype.showEntity = function (id) {
-    this.managers.render.show(id);
+    this._managers.render.show(id);
   };
 
   Atlas.prototype.hideEntity = function (id) {
-    this.managers.render.hide(id);
+    this._managers.render.hide(id);
   };
 
   return Atlas;
