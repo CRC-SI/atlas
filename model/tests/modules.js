efc1645be8d2618a33f4de59283cf991b1585f2d
Brendan Studds <brendan.studds@gmail.com>
2013-12-06 15:13:29 +1100

    added new tests to modules files.

--- model/tests/modules.js
+++ model/tests/modules.js
@@ -1,11 +1,11 @@
 define([ 
-  'atlas/model/tests/GeoEntity',
-  'atlas/model/tests/Feature',
   /*=====
   'atlas/model/tests/Network',
   'atlas/model/tests/PointHandle',
-  'atlas/model/tests/Polygon',
-  =====*/
   'atlas/model/tests/Line',
+  =====*/
+  'atlas/model/tests/Feature',
+  'atlas/model/tests/Polygon',
+  'atlas/model/tests/GeoEntity',
   'atlas/model/tests/Vertex'
 ], 1);
