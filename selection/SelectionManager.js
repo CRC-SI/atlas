3488e801043f8894bacd32d22857d9a7c98e5ebb
Brendan Studds <brendan.studds@gmail.com>
2013-12-11 18:36:46 +1100

    Created basic selection manager that can handle selecting entities via clicks (not integrated to MUtopia).

--- selection/SelectionManager.js
+++ selection/SelectionManager.js
@@ -1,1 +1,135 @@
-// SelectionManager.js
+define([
+  'atlas/util/DeveloperError'
+], function (DeveloperError) {
+
+  /**
+   * Constructs a new SelectionManager object.
+   * @class The SelectionManager maintains a list of the currently
+   * selected GeoEntities. It exposes an API to select and deselect
+   * inidividual GeoEntities or a set of entities either specified
+   * explicitly or by specifying a geographic area to select from.
+   *
+   * @param {Object} atlasManagers - A reference to the Atlas manager objects.
+   * @returns {atlas/selection/SelectionManager}
+   *
+   * @alias atlas/selection/SelectionManager
+   * @constructor
+   */
+  var SelectionManager = function (atlasManagers) {
+    /**
+     * Contains a map of entity ID to entity of all selected entities.
+     * @type {Object}
+     */
+    this._selectedEntities = {};
+
+    /**
+     * Contains references to all of the currently defined Atlas manager
+     * objects.
+     * @type {Object}
+     */
+    this._atlasManagers = atlasManagers;
+    this._atlasManagers.selection = this;
+  };
+
+
+  /**
+   * Registers event handlers with the EventManager for relevant events.
+   */
+  SelectionManager.prototype.bindEvents = function () {
+    // Create event handlers for pertinent events.
+    var handlers = [
+      {
+        source: 'intern',
+        name: 'entity/click',
+        callback: function (name, args) {
+          if (args) {
+            this.selectEntity(args.entity, args.modifieds.contains('shift'));
+          }
+        }.bind(this)
+      },
+      {
+        source: 'intern',
+        name: 'entity/remove',
+        callback: function (name, args) {
+          // If the Entity has been removed, don't need to deselect it, just remove it from _selectedEntities.
+          delete this._selectedEntities[args.id];
+        }.bind(this)
+      }
+    ];
+    // Register event handlers with the EventManager.
+    this._atlasManagers.event.addEventHandlers(handlers);
+  };
+
+  /**
+   * Causes an Entity to become selected.
+   * @param {String} id - The ID of the GeoEntity to select.
+   * @param {Boolean} [keepSelection=false] - If true, the existing selection will be added to rather than cleared.
+   */
+  SelectionManager.prototype.selectEntity = function (id, keepSelection) {
+    console.debug('selecting entity', id);
+    if (!keepSelection) {
+      this.clearSelection();
+    }
+    var entity = this._atlasManagers.render.getEntity(id);
+    if (entity) {
+      this._selectedEntities[entity._id] = entity;
+      entity.select();
+    }
+    console.debug('selected entity', id);
+  };
+
+  /**
+   * Removes the given Entity from the current selection.
+   * @param {String} id - The ID of the GeoEntity to deselect.
+   */
+  SelectionManager.prototype.deselectEntity = function (id) {
+    if (id in this._selectedEntities) {
+      this._selectedEntities[id].deselect();
+      delete this._selectedEntities[id];
+    }
+  };
+
+  /**
+   * Deselects all currently selected GeoEntities.
+   */
+  SelectionManager.prototype.clearSelection = function () {
+    console.debug('clearing selection', this._selectedEntities);
+    for (var id in this._selectedEntities) {
+      if (this._selectedEntities.hasOwnProperty(id)) {
+        this._selectedEntities[id].deselect();
+      }
+    }
+    this._selectedEntities = {};
+    console.debug('cleared selection');
+  };
+
+  /**
+   * Selects multiple GeoEntities.
+   * @param {Array.<String>} ids - The IDs of all GeoEntities to be selected.
+   * @param {Boolean} [keepSelection=false] - If true, the existing selection will be added to rather than cleared.
+   */
+  SelectionManager.prototype.selectEntities = function (ids, keepSelection) {
+    console.debug('selecting entities', ids);
+    if (!keepSelection) {
+      this.clearSelection();
+    }
+    for (var i = 0; i < ids.length; i++) {
+      var entity = this._atlasManagers.render.getEntity(ids[i]);
+      entity.select();
+      this._selectedEntities[entity._id] = entity;
+    }
+    console.debug('selected entities', ids);
+  };
+
+  /**
+   * Selects multiple GeoEntities which are contained by the given Polygon.
+   * @param {altas/model/Polygon} boundingBox - The polygon defining the area to select GeoEntities.
+   * @param {Boolean} [intersects=false] - If true, GeoEntities which intersect but are not contained by the <code>boundingBox</code> are also selected.
+   * @param {Boolean} [keepSelection=false] - If true, the existing selection will be added to rather than cleared.
+   */
+  SelectionManager.prototype.selectWithinPolygon = function () {
+    throw new 'No idea how to do this yet.';
+  };
+
+  return SelectionManager;
+});
---
+++ selection/tests/SelectionManager.js
@@ -0,0 +1,167 @@
+define([
+  'doh/runner',
+  'dam/TestCase',
+  /* Code under test */
+  '../SelectionManager',
+], function (doh, TestCase, SelectionManager) {
+
+  var atlasManagers;
+  var selectionManager;
+  
+  var mockEntity = function (id) {
+    var entity = {
+      _id: id,
+      selected: false,
+      select: function () {
+        this.selected = true;
+      },
+      deselect: function () {
+        this.selected = false;
+      }
+    };
+    return entity;
+  }
+  
+  var entity1 = new mockEntity(1);
+  var entity2 = new mockEntity(2);
+  var entity3 = new mockEntity(3);
+  var entity4 = new mockEntity(4);
+  
+  var mockRenderManager = function () {
+    var rm = {
+      es: {},
+      getEntity: function (id) {
+        return this.es[id];
+      }
+    }
+    rm.es[entity1._id] = entity1;
+    rm.es[entity2._id] = entity2;
+    rm.es[entity3._id] = entity3;
+    rm.es[entity4._id] = entity4;
+    return rm;
+  }
+
+  /* Begin test case definitions */
+  new TestCase({
+
+    name: 'atlas/selection/SelectionManager',
+
+    setUp: function () {
+      // summary:
+      atlasManagers = {
+        dom: {},
+        render: mockRenderManager(),
+        event: {},
+        selection: {}
+      };
+      selectionManager = new SelectionManager(atlasManagers);
+    },
+
+    tearDown: function () {
+      atlasManagers = {
+        dom: {},
+        event: {},
+        render: {},
+        selection: {}
+      };
+    },
+
+    testCreate: function () {
+      doh.t(selectionManager instanceof SelectionManager, 'Did not create a SelectionManager');
+      doh.is(selectionManager, atlasManagers.selection, 'Reference not stored correctly');
+    },
+    
+    testSelectEntity: function () {
+      selectionManager.selectEntity(entity1._id, false);
+      doh.is(entity1, selectionManager._selectedEntities[entity1._id], 'Entity not added to selection correctly.');
+      doh.t(entity1.selected, 'Entity was not actually selected');
+    },
+    
+    testSelectEntity_KeepSelection: function () {
+      selectionManager.selectEntity(entity1._id, true);
+      selectionManager.selectEntity(entity2._id, true);
+      selectionManager.selectEntity(entity3._id, true);
+      selectionManager.selectEntity(entity4._id, true);
+      doh.is(entity1, selectionManager._selectedEntities[entity1._id], 'Entity not added to selection correctly.');
+      doh.is(entity2, selectionManager._selectedEntities[entity2._id], 'Entity not added to selection correctly.');
+      doh.is(entity3, selectionManager._selectedEntities[entity3._id], 'Entity not added to selection correctly.');
+      doh.is(entity4, selectionManager._selectedEntities[entity4._id], 'Entity not added to selection correctly.');
+      doh.t(entity1.selected, 'Entity was not actually selected');
+      doh.t(entity2.selected, 'Entity was not actually selected');
+      doh.t(entity3.selected, 'Entity was not actually selected');
+      doh.t(entity4.selected, 'Entity was not actually selected');
+    },
+    
+    testSelectEntity_ClearSelection: function () {
+      selectionManager.selectEntity(entity1._id, true);
+      selectionManager.selectEntity(entity2._id, true);
+      selectionManager.selectEntity(entity3._id, true);
+      selectionManager.selectEntity(entity4._id, false);
+      doh.f(selectionManager._selectedEntities[entity1._id], 'Entity not deselected correctly.');
+      doh.f(selectionManager._selectedEntities[entity2._id], 'Entity not deselected correctly.');
+      doh.f(selectionManager._selectedEntities[entity3._id], 'Entity not deselected correctly.');
+      doh.is(entity4, selectionManager._selectedEntities[entity4._id], 'Entity not added to selection correctly.');
+      doh.f(entity1.selected, 'Entity was not actually deselected');
+      doh.f(entity2.selected, 'Entity was not actually deselected');
+      doh.f(entity3.selected, 'Entity was not actually deselected');
+      doh.t(entity4.selected, 'Entity was not actually selected');
+    },
+    
+    testSelectEntity_WrongID: function () {
+      selectionManager.selectEntity(12345, false);
+      doh.f(selectionManager._selectedEntities[12345], 'Non-existing entity "selected"');
+    },
+    
+    testSelectEntities: function () {
+      selectionManager.selectEntities([entity1._id, entity2._id, entity3._id, entity4._id]);
+      doh.is(entity1, selectionManager._selectedEntities[entity1._id], 'Entity not added to selection correctly.');
+      doh.is(entity2, selectionManager._selectedEntities[entity2._id], 'Entity not added to selection correctly.');
+      doh.is(entity3, selectionManager._selectedEntities[entity3._id], 'Entity not added to selection correctly.');
+      doh.is(entity4, selectionManager._selectedEntities[entity4._id], 'Entity not added to selection correctly.');
+      doh.t(entity1.selected, 'Entity was not actually selected');
+      doh.t(entity2.selected, 'Entity was not actually selected');
+      doh.t(entity3.selected, 'Entity was not actually selected');
+      doh.t(entity4.selected, 'Entity was not actually selected');
+    },
+    
+    testSelectEntities_KeepSelection: function () {
+      selectionManager.selectEntity(entity1._id);
+      selectionManager.selectEntities([entity2._id, entity3._id, entity4._id], true);
+      doh.is(entity1, selectionManager._selectedEntities[entity1._id], 'Entity1 not added to selection correctly.');
+      doh.is(entity2, selectionManager._selectedEntities[entity2._id], 'Entity2 not added to selection correctly.');
+      doh.is(entity3, selectionManager._selectedEntities[entity3._id], 'Entity3 not added to selection correctly.');
+      doh.is(entity4, selectionManager._selectedEntities[entity4._id], 'Entity4 not added to selection correctly.');
+      doh.t(entity1.selected, 'Entity1 was not actually selected');
+      doh.t(entity2.selected, 'Entity2 was not actually selected');
+      doh.t(entity3.selected, 'Entity3 was not actually selected');
+      doh.t(entity4.selected, 'Entity4 was not actually selected');
+    },
+    
+    testSelectEntities_ClearSelection1: function () {
+      selectionManager.selectEntity(entity1._id);
+      selectionManager.selectEntities([entity2._id, entity3._id, entity4._id], false);
+      doh.f(selectionManager._selectedEntities[entity1._id], 'Entity1 not deselected correctly.');
+      doh.is(entity2, selectionManager._selectedEntities[entity2._id], 'Entity2 not added to selection correctly.');
+      doh.is(entity3, selectionManager._selectedEntities[entity3._id], 'Entity3 not added to selection correctly.');
+      doh.is(entity4, selectionManager._selectedEntities[entity4._id], 'Entity4 not added to selection correctly.');
+      doh.f(entity1.selected, 'Entity1 was not deselected');
+      doh.t(entity2.selected, 'Entity2 was not actually selected');
+      doh.t(entity3.selected, 'Entity3 was not actually selected');
+      doh.t(entity4.selected, 'Entity4 was not actually selected');
+    },
+    
+    testSelectEntities_ClearSelection2: function () {
+      selectionManager.selectEntity(entity1._id);
+      selectionManager.selectEntities([entity2._id, entity3._id, entity4._id]);
+      doh.f(selectionManager._selectedEntities[entity1._id], 'Entity1 not deselected correctly.');
+      doh.is(entity2, selectionManager._selectedEntities[entity2._id], 'Entity2 not added to selection correctly.');
+      doh.is(entity3, selectionManager._selectedEntities[entity3._id], 'Entity3 not added to selection correctly.');
+      doh.is(entity4, selectionManager._selectedEntities[entity4._id], 'Entity4 not added to selection correctly.');
+      doh.f(entity1.selected, 'Entity1 was not deselected');
+      doh.t(entity2.selected, 'Entity2 was not actually selected');
+      doh.t(entity3.selected, 'Entity3 was not actually selected');
+      doh.t(entity4.selected, 'Entity4 was not actually selected');
+    }        
+  }).register(doh);
+});
+
