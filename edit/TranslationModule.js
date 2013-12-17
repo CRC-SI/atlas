78fd525deff0a044bfbd0e5fe154ee3cce47ecfa
Aram Kocharyan <akarmenia@gmail.com>
2013-12-17 20:55:23 +1100

    Implemented translation manager.

--- edit/TranslationModule.js
+++ edit/TranslationModule.js
@@ -1,7 +1,8 @@
 define([
   'atlas/util/Extends',
-  './EditModule'
-], function (extend, EditModule) {
+  './EditModule',
+  'atlas/model/GeoEntity'
+], function (extend, EditModule, GeoEntity) {
 
   /**
    * Constructs a new TranslationModule.
@@ -10,9 +11,9 @@ define([
    * @alias atlas/edit/TranslationModule
    * @constructor
    */
-  var TranslationModule = function () {
-    this._entities = {};
-    this._originalEntityLocations = {};
+  var TranslationModule = function (atlasManagers) {
+    this._reset();
+    this._atlasManagers = atlasManagers;
   };
 
   // Inherit from TranslationModule.
@@ -20,38 +21,63 @@ define([
 
   // Stores Entities being translated, initial location
   TranslationModule.prototype.start = function (name, event) {
-    // PSUEDO CODE
-    /* lastLocation = here
-     * for each selected entity
-     *    store current entity location locally
-     */
+    var target = event.target;
+    if (!(target instanceof GeoEntity)) {
+      return;
+    }
+    var id = target.id;
+    // TODO(aramk) define where here is by reading fields from the event object.
+    this._originalLocation = this._lastLocation = here;
+    var selection = this._atlasManagers.selection.getSelection();
+    // TODO(aramk) verify this is the field containing the clicked on entity.
+    if (selection[id] !== undefined) {
+      this._entities = selection;
+    } else {
+      this._entities[id] = target;
+    }
   };
 
   // Updates 'initial' location, renders entities at current location
   // Have function on GeoEntity to temporarily change location data and re-render
   TranslationModule.prototype.update = function (name, event) {
-    // PSUEDO CODE
-    /* currentLocation = here
-     * diff = determineLocationChange
-     * for each selected entity
-     *    entity.move(diff);
-     *    entity.show()
-     * clear drag state
-     * lastLocation = here
-     */
+    var currentLocation = here;
+    // TODO(aramk) use the actual structure for coords.
+    this._move(this._lastLocation, currentLocation);
+    this._lastLocation = currentLocation;
   };
 
   // Changes actual location of entities, renders them there.
   // Have function on GeoEntity to permantely update location data and re-render
   TranslationModule.prototype.end = function (name, event) {
-    // PSUEDO CODE
-    /* diff = determineLocationChange
-     * for each selected entity
-     *    entity.move(diff);
-     *    entity.show()
-     * clear drag state
-     */
+    this.update(name, event);
+    this._reset();
   };
+
+  TranslationModule.prototype.cancel = function (name, event) {
+    this._move(this._lastLocation, this._originalLocation);
+    this._reset();
+  };
+
+  TranslationModule.prototype._move = function (oldCoord, newCoord) {
+    var diffLocation = diffCoord(oldCoord, newCoord);
+    for (var entity in this._entities) {
+      // TODO(aramk) this assumes that move takes a location as a diff, not absolute value.
+      entity.move(diffLocation);
+      // TODO(aramk) shouldn't move already render the model?
+      entity.show();
+    }
+  };
+
+  TranslationModule.prototype._reset = function () {
+    this._entities = {};
+    this._lastLocation = null;
+    this._originalLocation = null;
+  };
+
+  // TODO(aramk) where possible, refactor common arithmetic into a class if one doesn't already exist.
+  function diffCoord(x, y) {
+    return [y[0] - x[0], y[1] - x[1]];
+  }
 
   return TranslationModule;
 });
