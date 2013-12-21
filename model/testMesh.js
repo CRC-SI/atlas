692b968285a76b2a04273e89a15b4e12380b925d
Brendan Studds <brendan.studds@gmail.com>
2013-12-22 10:43:12 +1100

    Added reference to source (pull request #8).

--- model/testMesh.js
+++ model/testMesh.js
@@ -1,12 +1,17 @@
 define([
   
 ], function () {
+  /**
+   * @fileOverview The position and indices data used here is copied from a Cesium example.
+   * @see {@link https://github.com/AnalyticalGraphicsInc/cesium/wiki/Geometry-and-Appearances}
+   */
+
   var negativeRootTwoOverThree = -Math.sqrt(2.0) / 3.0;
   var negativeOneThird = -1.0 / 3.0;
   var rootSixOverThree = Math.sqrt(6.0) / 3.0;
 
   
-  var mesh = {
+  return  {
     id: 'id',
     color: [255,255,255,0], /* red, green, blue, alpha */
     geoLocation: [-37.9, 144.5, 0], /* lat, lng, elevation */
@@ -25,8 +30,6 @@ define([
       rootSixOverThree, negativeRootTwoOverThree, negativeOneThird
     ],
     indices: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11], /* Triangles in mesh, each element specifies an index into the position array */
-    normals: [1,2,3], /* Normals per triangle vertex */
+    normals: [1,2,3] /* Normals per triangle vertex, ie 1-1 with indices, but not necessarily with positions. */
   };
-  
-  return mesh;
 });
