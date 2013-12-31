eb5aa9b8de81be20e53a6bc60265519ad172a0a0
Brendan Studds <brendan.studds@gmail.com>
2013-12-31 16:14:22 +1100

    Added some odds and sods to the test meshes.

--- model/testMesh.js
+++ model/testMesh.js
@@ -1,5 +1,5 @@
 define([
-  
+
 ], function () {
   /**
    * @fileOverview The position and indices data used here is copied from a Cesium example.
@@ -10,7 +10,7 @@ define([
   var negativeOneThird = -1.0 / 3.0;
   var rootSixOverThree = Math.sqrt(6.0) / 3.0;
 
-  
+
   return  {
     id: 'id',
     color: [255,255,255,0], /* red, green, blue, alpha */
@@ -29,7 +29,7 @@ define([
       0.0, (2.0 * Math.sqrt(2.0)) / 3.0, negativeOneThird,
       rootSixOverThree, negativeRootTwoOverThree, negativeOneThird
     ],
-    indices: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11], /* Triangles in mesh, each element specifies an index into the position array */
+    triangles: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11], /* Triangles in mesh, each element specifies an index into the position array */
     normals: [1,2,3] /* Normals per triangle vertex, ie 1-1 with indices, but not necessarily with positions. */
   };
 });
