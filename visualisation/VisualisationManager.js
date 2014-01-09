65e4ca00f41073e683d632f220986b8e2af7ff6f
Brendan Studds <brendan.studds@gmail.com>
2014-01-09 16:39:11 +1100

    creating the Projection object.

--- visualisation/VisualisationManager.js
+++ visualisation/VisualisationManager.js
@@ -1,5 +1,21 @@
 define([
 ], function () {
+  /**
+   * An object describing a projection.
+   * @typedef {Object} Projection
+   * @property {String} artifact - The artifact to project onto, currently only 'height' supported.
+   * @property {String} projection - The type of projection, currently only 'continuous' supported.
+   * @property {Object.<String, Number>} values - A map of Entity ID to the values to be projected for the Entity.
+   * @property {Object.<String, Object>} effects - A map of Entity ID to the effect the projection has.
+   * @property {Object.<String, Object>} effects.oldVal - The value of an Entity's artifact before the projection was applied.
+   * @property {Object.<String, Object>} effects.newVal - The value of an Entity's artifact after the projection was applied.
+   * @property {Object} stats - Statistical data of the values.
+   * @property {Number} stats.min - The minimum value.
+   * @property {Number} stats.max - The maximum value.
+   * @property {Number} stats.sum - The sum of all values.
+   * @property {Number} stats.average - The average of the values.
+   * @property {Object} options - Options to configure the exact behaviour of the projection.
+   */
 
   /**
    * Constructs a new VisualisationManager
@@ -7,6 +23,13 @@ define([
    * @constructor
    */
   var VisualisationManager = function (atlasManagers) {
+    this._atlasManagers = atlasManagers;
+
+    /**
+     * The defined projections currently effecting Atlas.
+     * @type {Object.<String, Projection>}
+     * @private
+     */
     this._projections = {};
     this._effectedEntities = {};
   };
@@ -17,15 +40,34 @@ define([
   /**
    * Takes a given artifact mapping, generates a projection and applies it to currently
    * rendered entities.
-   * @param {Object} artifactMapping - An object describing the artifact and projection.
+   * @param {Object} args - An object describing the projection.
+   * @param {String} args.artifact - The artifact to project onto, currently only 'height' supported.
+   * @param {String} args.type - The type of projection, currently only 'continuous' supported.
+   * @param {Object.<String, Number>} args.values - A map of Entity ID to the value to be projected for the Entity.
+   * @param {Object} [args.options] - Options to configure the exact behaviour of the projection.
+   * @returns {Projection|undefined} The Projection object, required to remove the Projection.
    */
-  VisualisationManager.prototype.addProjection = function (artifactMapping) {
-    if (!(artifactMapping.artifact in VisualisationManager.SUPPORTED_ARTIFACTS)) { return; }
+  VisualisationManager.prototype.addProjection = function (args) {
+    if (!(args.artifact in this._artifactRenderers)) { return undefined; }
+    // TODO(bpstudds): Get the check for projection type working.
+    //if (VisualisationManager.SUPPORTED_PROJECTIONS.indexOf(args.type) != -1) { return; }
 
     // Remove any projection already on this artifact.
-    if (artifactMapping.artifact in this._projections) {
-      this.removeProjection(this._projections[artifactMapping.artifact]);
+    if (args.artifact in this._projections) {
+      this.removeProjection(this._projections[args.artifact]);
     }
+    var projection = {};
+    projection.artifact = args.artifact;
+    projection.type = args.type;
+    projection.options = {};
+    projection.effects = {};
+    var datas = this._calculateProjectedValues(args);
+    projection.stats = datas.stats;
+    projection.values = datas.values;
+    this._projections[projection.artifact] = projection;
+    this._renderProjection(projection);
+
+    return projection;
   };
 
   /**
