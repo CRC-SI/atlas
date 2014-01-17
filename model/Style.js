08f0d248788e514f4d258bf12279011f37bedc7c
Brendan Studds <brendan.studds@gmail.com>
2014-01-17 15:13:37 +1100

    Add getters and setters to Style, add modifyStyle to Feature which updates it's style and propagates that to it's components, change Style members to private, updated jsdoc (CAT-77).

--- model/Style.js
+++ model/Style.js
@@ -1,7 +1,8 @@
 // Style.js
 define([
+  'atlas/util/DeveloperError',
   './Colour'
-], function (Colour) {
+], function (DeveloperError, Colour) {
   "use strict";
 
   /**
@@ -19,9 +20,10 @@ define([
   var Style = function (fillColour, borderColour, borderWidth) {
     /**
      * Fill colour of this Style.
-     * @type {Colour}
+     * @type {atlas.model.Colour}
+     * @private
      */
-    this.fillColour = fillColour || Colour.GREEN;
+    this._fillColour = fillColour || Colour.GREEN;
 
     /**
      * Fill opacity of this Style.
@@ -31,9 +33,10 @@ define([
 
     /**
      * Border colour of this Style.
-     * @type {Colour}
+     * @type {atlas.model.Colour}
+     * @private
      */
-    this.borderColour = fillColour || Colour.GREEN;
+    this._borderColour = fillColour || Colour.GREEN;
 
     /**
      * Border opacity of this Style.
@@ -44,8 +47,70 @@ define([
     /**
      * Border width in pixels of this Style.
      * @type {Number}
+     * @private
      */
-    this.borderWidth = borderWidth || 1.0;
+    this._borderWidth = borderWidth || 1.0;
+  };
+
+  /**
+   * Sets the Style's fill colour.
+   * @param {atlas.model.Colour} colour - The new fill colour.
+   * @returns {atlas.model.Colour} The original colour.
+   */
+  Style.prototype.setFill = function (colour) {
+    if (!(colour instanceof Colour)) {
+      throw new DeveloperError('Feature fill colour only accepts an atlas.model.colour, not', colour);
+    }
+    var original = this._fillColour;
+    this._fillColour = colour;
+    return original;
+  };
+
+  /**
+   * Sets the Style's border colour.
+   * @param {atlas.model.Colour} colour - The new border colour.
+   * @returns {atlas.model.Colour} The original border colour.
+   */
+  Style.prototype.setBorderColour = function (colour) {
+    if (!(colour instanceof Colour)) {
+      throw new DeveloperError('Feature border colour only accepts an atlas.model.colour, not', colour);
+    }
+    var original = this._borderColour;
+    this._borderColour = colour;
+    return original;
+  };
+
+  /**
+   * Sets the Style's border width.
+   * @param {Number} width - The new border width, in pixels.
+   * @returns {Number} The original border width.
+   */
+  Style.prototype.setBorderWidth = function (width) {
+    width = parseInt(width, 10) || 1;
+    var original = this._borderWidth;
+    this._borderWidth = width;
+    return original;
+  };
+
+  /**
+   * @returns {atlas.model.Colour} The Style's fill colour.
+   */
+  Style.prototype.getFill = function () {
+    return this._fillColour;
+  };
+
+  /**
+   * @returns {atlas.model.Colour} The Style's border colour.
+   */
+  Style.prototype.getBorderColour = function () {
+    return this._borderColour;
+  };
+
+  /**
+   * @returns {Number} The Style's border width, in pixels.
+   */
+  Style.prototype.getBorderWidth = function () {
+    return this._borderWidth;
   };
 
   Style.DEFAULT = new Style(Colour.GREEN, Colour.GREEN, 1);
