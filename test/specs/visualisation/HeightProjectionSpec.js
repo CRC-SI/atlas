9b6acee0f731feb0bc4fba561f900ece93bdce10
Brendan Studds <brendan.studds@gmail.com>
2014-01-30 11:44:53 +1100

    Added to the HeightProjection test spec (needs moar tests) (CAT-78).

--- test/specs/visualisation/HeightProjectionSpec.js
+++ test/specs/visualisation/HeightProjectionSpec.js
@@ -1,17 +1,18 @@
 define([
+  'atlas/model/mixin',
   'atlas/model/Feature',
   // Code under test
   'atlas/visualisation/HeightProjection'
-], function (Feature, HeightProjection) {
+], function (mixin, Feature, HeightProjection) {
   describe('A HeightProjection', function () {
-    var heightProj;
-    var someValues = {0: 0, 1: 1, 2: 2};
-    var initialHeight = 50;
-    var someEntities = {
-      0: new Feature(0, {id: 0, height: initialHeight}),
-      1: new Feature(1, {id: 1, height: initialHeight}),
-      2: new Feature(2, {id: 1, height: initialHeight})
-    };
+    var heightProj,
+        someValues = {0: 0, 1: 1, 2: 2},
+        initialHeight = 50,
+        someEntities = {
+          0: new Feature(0, {id: 0, height: initialHeight}),
+          1: new Feature(1, {id: 1, height: initialHeight}),
+          2: new Feature(2, {id: 1, height: initialHeight})
+        };
 
     beforeEach(function () {
       heightProj = new HeightProjection({values: someValues, entities: someEntities});
@@ -28,31 +29,48 @@ define([
 
     describe('can render effects', function () {
 
-      it('to all it\'s entities', function () {
-        heightProj.render();
-        for (var id in Object.keys(someEntities)) {
+      describe('with a default codomain and bin', function () {
+        it('to all it\'s entities', function () {
+          heightProj.render();
+          for (var id in Object.keys(someEntities)) {
+            expect(someEntities[id].setHeight).toHaveBeenCalledWith(heightProj._effects[id].newValue);
+            expect(someEntities[id].setHeight.calls.length).toEqual(1);
+          }
+        });
+
+        it('to a subset of entities', function () {
+          var ids = [0, 2];
+          heightProj.render(ids);
+          for (var i = 0; i < ids.length; i++) {
+            var id = ids[i];
+            expect(someEntities[id].setHeight).toHaveBeenCalledWith(heightProj._effects[id].newValue);
+            expect(someEntities[id].setHeight.calls.length).toEqual(1);
+          }
+        });
+
+        it('to a single entity', function () {
+          var id = 1;
+          heightProj.render(id);
           expect(someEntities[id].setHeight).toHaveBeenCalledWith(heightProj._effects[id].newValue);
           expect(someEntities[id].setHeight.calls.length).toEqual(1);
-        }
-      });
+        });
+      }); // End 'with default codomain and bin'.
 
-      it('to a subset of entities', function () {
-        var ids = [0, 2];
-        heightProj.render(ids);
-        for (var i = 0; i < ids.length; i++) {
-          var id = ids[i];
-          expect(someEntities[id].setHeight).toHaveBeenCalledWith(heightProj._effects[id].newValue);
+      describe('with', function () {
+        var args = {values: someValues, entities: someEntities};
+
+        it('a fixed codomain', function () {
+          heightProj = new HeightProjection(mixin(args, {codomain: {fixedProj: 15}}));
+          expect(someEntities[id].setHeight).toHaveBeenCalledWith(15);
           expect(someEntities[id].setHeight.calls.length).toEqual(1);
-        }
-      });
+        });
 
-      it('to a single entity', function () {
-        var id = 1;
-        heightProj.render(id);
-        expect(someEntities[id].setHeight).toHaveBeenCalledWith(heightProj._effects[id].newValue);
-        expect(someEntities[id].setHeight.calls.length).toEqual(1);
-      });
-    }); // End 'can render effects'
+        it('a single codomain and (default) bin', function () {
+          fail();
+        });
+
+      }); // End 'with specific codomain'.
+    }); // End 'can render effects'.
 
     describe('can unrender effects', function() {
       beforeEach(function () {
@@ -78,13 +96,12 @@ define([
         }
       });
 
-
       it('on all it\'s entities', function () {
         var id = 1;
         heightProj.unrender(id);
         expect(someEntities[id].setHeight).toHaveBeenCalledWith(initialHeight);
         expect(someEntities[id].setHeight.calls.length).toEqual(2);
-      });
-    }); // End 'can unrender effects'
+      }); // 'End can unrender effects'.
+    }); // End 'can unrender effects'.
   }); // End 'A HeightProjection'.
 });
