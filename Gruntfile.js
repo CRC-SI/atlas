832b6b9871d156352129ebbb027c6fb2e5afb243
Brendan Studds <brendan.studds@gmail.com>
2014-01-31 12:52:15 +1100

    Include new library keycode.js in build toolchain.

--- Gruntfile.js
+++ Gruntfile.js
@@ -36,7 +36,8 @@ module.exports = function(grunt) {
      bowerDep: {
        files: [
          {src: './lib/Requirejs/require.js', dest: './lib/require.js'},
-         {src: './lib/Openlayers/index.js', dest: './lib/open-layers.js'}
+         {src: './lib/Openlayers/index.js', dest: './lib/open-layers.js'},
+         {src: './lib/Keycode/keycode.js', dest: './lib/keycode.js'}
        ]
      }
     }
