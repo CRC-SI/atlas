b7069cde086c655362ea0e687b13cb87a4660cb2
Brendan Studds <brendan.studds@gmail.com>
2014-03-19 17:19:23 +1100

    WIP. Changing how Atlas Handle works as current version won't. Started defining unit tests for Atlas-cesium Handle.

--- src/model/Handle.js
+++ src/model/Handle.js
@@ -1,30 +1,45 @@
 define([
+  'atlas/util/DeveloperError',
+  'atlas/util/mixin',
+  'atlas/model/Vertex',
   // Base class
   'atlas/model/GeoEntity'
-], function (GeoEntity) {
+], function (DeveloperError, mixin, Vertex, GeoEntity) {
   /**
-   * @classdesc The Handle class is an interactive vector linked to a collection
-   * of GeoEntities (typically one). The Handle provides an interface between the editing
-   * subsystem and GeoEntities. When a handle is modified, the Handle delegates these calls
+   * @classdesc The Handle class is an interactive vector linked to a GeoEntity or Vertex.
+   * The Handle provides an interface between the editing subsystem and GeoEntities.
+   * When a handle is modified, the Handle delegates these calls
    * to the linked GeoEntities.
-   *
+   * @param {atlas.model.GeoEntity} args.target - The owner of the linked <code>Vertex</code>.
+   * @param {atlas.model.Vertex | atlas.model.GeoEntity} args.linked - The Vertex or GeoEntity that is linked to the Handle.
    * @class atlas.model.Handle
    * @extends atlas.model.GeoEntity
    */
-  return GeoEntity.extend( /** @lends atlas.model.Handle# */ {
+  var Handle;
+  return mixin(Handle = GeoEntity.extend( /** @lends atlas.model.Handle# */ {
+
     /**
-     * A array of the linked GeoEntities.
-     * @type {Array.<atlas.model.GeoEntity}
+     * ID of the Handle.
+     * @type {String}
+     * @protected
+     */
+    _id: null,
+
+    /**
+     * The linked GeoEntity or Vertex.
+     * @type {atlas.model.GeoEntity | atlas.model.Vertex}
+     * @protected
      */
     _linked: null,
 
-    _init: function (targets) {
-      if (targets.getId) {
-        // targets is a single GeoEntity, convert it to an array.
-        var target = targets;
-        targets = [target];
+    _init: function (args) {
+      if (!args.linked) {
+        throw new DeveloperError('Can not create Handle without linked entity.');
+      } else if (args.linked instanceof Vertex && !args.target) {
+        throw new DeveloperError('Must specify target of Handle if linked to a Vertex.');
       }
-      this._linked = targets;
+      this._linked = args.linked;
+      this._id = Handle.getNextId();
     },
 
     remove: function () {
@@ -46,9 +61,7 @@ define([
     // MODIFIERS
     // -------------------------------------------
     _delegateToLinked: function (method, args) {
-      this.getLinked().forEach(function (entity) {
-        entity[method].apply(entity, args);
-      })
+      this.getLinked().apply(this.getLinked(), args);
     },
 
     rotate: function () {
@@ -62,5 +75,22 @@ define([
     translate: function () {
       this._delegateToLinked('translate', arguments);
     }
-  });
+  }),
+    {
+      // -------------------------------------------
+      // STATICS
+      // -------------------------------------------
+
+      /**
+       * ID to assign to the next created Handle.
+       * @type {Number}
+       * @protected
+       */
+      _nextId: 100000,
+
+      getNextId: function () {
+        return 'handle' + Handle._nextId++;
+      }
+    }
+  );
 });
--- test/specs/model/HandleSpec.js
+++ test/specs/model/HandleSpec.js
@@ -1,40 +1,69 @@
 define([
   'atlas/model/GeoEntity',
+  'atlas/model/Vertex',
   // Code under test
   'atlas/model/Handle'
-], function (GeoEntity, Handle) {
+], function (GeoEntity, Vertex, Handle) {
   describe ('A Handle', function () {
     var handle,
         entity1,
         entity2,
+        vertex,
         modifiers = ['translate', 'rotate', 'scale'];
 
     beforeEach (function () {
       entity1 = new GeoEntity({id: '001'});
       entity2 = new GeoEntity({id: '002'});
+      vertex = new Vertex({x: 0, y: 0, z: 0});
       [entity1, entity2].forEach(function (entity) {
         modifiers.forEach(function (method) {
           spyOn(entity, method);
         });
-      })
+      });
+      spyOn(vertex, 'translate');
     });
 
     afterEach (function () {
       handle = null;
       entity1 = null;
+      entity2 = null;
+      vertex = null;
     });
 
     describe ('can be constructed', function () {
-      it ('with a single target', function () {
+      it ('with a GeoEntity', function () {
         handle = new Handle(entity1);
         expect(handle).not.toBeNull();
-        expect(handle.getLinked()).toEqual([entity1]);
+        expect(handle.getLinked()).toEqual(entity1);
+        expect(handle._id).toEqual('handle' + Handle._nextId);
       });
 
-      it ('with multiple targets', function () {
+      it ('with a Vertex', function () {
+        handle = new Handle(vertex);
+        expect(handle).not.toBeNull();
+        expect(handle.getLinked()).toEqual(vertex);
+        expect(handle._id).toEqual('handle' + Handle._nextId);
+      });
+
+      // Not any more
+      xit ('with multiple targets', function () {
         handle = new Handle([entity1, entity2]);
         expect(handle).not.toBeNull();
         expect(handle.getLinked()).toEqual([entity1, entity2]);
+      });
+
+      it ('but fails without a target', function () {
+        var fail = function () {
+          return new Handle();
+        };
+        expect(fail).toThrow();
+      });
+
+      it ('but fails with a non-Vertex or GeoEntity target', function () {
+        var fail = function () {
+          return new Handle('string');
+        };
+        expect(fail).toThrow();
       })
     });
 
@@ -47,7 +76,7 @@ define([
         });
       });
 
-      it ('to multiple GeoEntities', function () {
+      xit ('to multiple GeoEntities', function () {
         handle = new Handle([entity1, entity2]);
         modifiers.forEach(function (method) {
           handle[method]();
