f115310d8c893b617e1dad6eff84ef0ce84a4d8a
Brendan Studds <brendan.studds@gmail.com>
2014-04-28 17:18:06 +1000

    Renamed all uses of FooStore to ItemStore.

--- test/specs/core/ItemStoreSpec.js
+++ test/specs/core/ItemStoreSpec.js
@@ -1,17 +1,17 @@
 define([
   // Code under test.
-  'atlas/core/FooStore'
-], function (FooStore) {
+  'atlas/core/ItemStore'
+], function (ItemStore) {
 
-  describe('A FooStore', function () {
+  describe('A ItemStore', function () {
 
-    var fooStore,
-        foos;
+    var itemStore,
+        items;
 
     beforeEach(function () {
-      foos = [];
+      items = [];
       [0, 1, 2, 3].forEach(function (id) {
-        foos.push({
+        items.push({
           id: id.toString(),
           val: id,
           getId: function () { return this.id; },
@@ -21,83 +21,83 @@ define([
     });
 
     afterEach(function () {
-      foos = null;
-      fooStore = null;
-    })
+      items = null;
+      itemStore = null;
+    });
 
     it('can be constructed', function () {
-      fooStore = new FooStore();
-      expect(fooStore._getterName).toEqual('getId');
-      expect(fooStore._items).toEqual({});
-      fooStore = new FooStore('fooIdGetterFunction');
-      expect(fooStore._getterName).toEqual('fooIdGetterFunction');
-      expect(fooStore._items).toEqual({});
-    })
+      itemStore = new ItemStore();
+      expect(itemStore._getterName).toEqual('getId');
+      expect(itemStore._items).toEqual({});
+      itemStore = new ItemStore('itemIdGetterFunction');
+      expect(itemStore._getterName).toEqual('itemIdGetterFunction');
+      expect(itemStore._items).toEqual({});
+    });
 
     describe('can do stuff', function () {
 
       beforeEach(function () {
-        fooStore = new FooStore();
+        itemStore = new ItemStore();
       });
 
       it('fails awesomely if a null id is added', function () {
-        var f = function () { fooStore.add({x: 0}); };
+        var f = function () { itemStore.add({x: 0}); };
         expect(f).toThrow();
-        expect(fooStore.getCount()).toBe(0);
+        expect(itemStore.getCount()).toBe(0);
       });
 
-      it('can have foos added', function () {
-        fooStore.add(foos[0]);
-        fooStore.add(foos[1]);
-        expect(fooStore._items['0']).toEqual(foos[0]);
-        expect(fooStore._items['1']).toEqual(foos[1]);
-        expect(fooStore.getCount()).toBe(2);
+      it('can have items added', function () {
+        itemStore.add(items[0]);
+        itemStore.add(items[1]);
+        expect(itemStore._items['0']).toEqual(items[0]);
+        expect(itemStore._items['1']).toEqual(items[1]);
+        expect(itemStore.getCount()).toBe(2);
       });
 
       it('can have multiple objects added', function () {
-        fooStore.addArray(foos);
+        itemStore.addArray(items);
         [0,1,2,3].forEach(function (id) {
-          expect(fooStore._items[id.toString()]).toEqual(foos[id]);
+          expect(itemStore._items[id.toString()]).toEqual(items[id]);
         });
-        expect(fooStore.getCount()).toBe(4);
+        expect(itemStore.getCount()).toBe(4);
       });
 
-      it('can retrieve foos', function () {
-        fooStore.add(foos[0]);
-        fooStore.add(foos[1]);
-        expect(fooStore.get('0')).toEqual(foos[0]);
-        expect(fooStore.get('1')).toEqual(foos[1]);
+      it('can retrieve items', function () {
+        itemStore.add(items[0]);
+        itemStore.add(items[1]);
+        expect(itemStore.get('0')).toEqual(items[0]);
+        expect(itemStore.get('1')).toEqual(items[1]);
       });
 
-      it('can have foos removed', function () {
-        fooStore.add(foos[0]);
-        fooStore.add(foos[1]);
-        expect(fooStore.getCount()).toBe(2);
-        fooStore.remove('0');
-        expect(fooStore.getCount()).toBe(1);
-        expect(fooStore._items['0']).toBeUndefined();
-        expect(fooStore._items['1']).toEqual(foos[1]);
+      it('can have items removed', function () {
+        itemStore.add(items[0]);
+        itemStore.add(items[1]);
+        expect(itemStore.getCount()).toBe(2);
+        itemStore.remove('0');
+        expect(itemStore.getCount()).toBe(1);
+        expect(itemStore._items['0']).toBeUndefined();
+        expect(itemStore._items['1']).toEqual(items[1]);
       });
 
-      it('safely fails to get a non-existent foo', function () {
-        fooStore.add(foos[1]);
-        fooStore.remove('1');
-        var nothing = fooStore.get('nothing'),
-            moreNothing = fooStore.get('1');
+      it('safely fails to get a non-existent item', function () {
+        itemStore.add(items[1]);
+        itemStore.remove('1');
+        var nothing = itemStore.get('nothing'),
+            moreNothing = itemStore.get('1');
         expect(nothing).toBeFalsy();
         expect(!nothing).toBe(true);
         expect(moreNothing).toBeFalsy();
         expect(!moreNothing).toBe(true);
       })
 
-      it('like apply a function to all foos', function () {
-        fooStore.add(foos[0]);
-        fooStore.add(foos[1]);
-        fooStore.add(foos[2]);
-        fooStore.add(foos[3]);
-        fooStore.map('incrVal');
+      it('like apply a function to all items', function () {
+        itemStore.add(items[0]);
+        itemStore.add(items[1]);
+        itemStore.add(items[2]);
+        itemStore.add(items[3]);
+        itemStore.map('incrVal');
         [0,1,2,3].forEach(function (id) {
-          expect(fooStore.get(id.toString()).val).toEqual(id + 1);
+          expect(itemStore.get(id.toString()).val).toEqual(id + 1);
         });
       });
     });
