0fe70f1a7c5606f24e8daace7752b670f3945490
Brendan Studds <brendan.studds@gmail.com>
2014-04-28 17:49:41 +1000

    Made testing better. Added a 'config' bit to the test-main.js file to toggle tests on and off. New test files need to be manually added though.

--- test/test-main.js
+++ test/test-main.js
@@ -1,23 +1,42 @@
 var tests = [];
-for (var file in window.__karma__.files) {
-  if (window.__karma__.files.hasOwnProperty(file)) {
-    if (/.*Spec\.js$/.test(file)) {
-      console.debug('test spec:', file);
-      tests.push(file);
-    }
+    specsConfig = [
+      { name: 'visualisation/AbstractProjection', run: true },
+      { name: 'visualisation/HeightProjection', run: true },
+      { name: 'visualisation/ColourProjection', run: true },
+      { name: 'visualisation/DynamicProjection', run: true },
+      { name: 'core/ItemStore', run: true },
+      { name: 'dom/Overlay', run: true },
+      { name: 'model/Handle', run: true },
+      { name: 'model/Ellipse', run: true },
+      { name: 'edit/EditManager', run: true },
+      { name: 'entity/EntityManager', run: true },
+      { name: 'camera/Camera', run: true },
+    ];
+
+specsConfig.forEach(function (config) {
+  if (config.run) {
+    tests.push('/base/atlas/test/specs/' + config.name + 'Spec.js');
   }
-}
+});
+
+//var tests = [];
+//for (var file in window.__karma__.files) {
+//  if (window.__karma__.files.hasOwnProperty(file)) {
+//    if (/.*Spec\.js$/.test(file)) {
+//      console.debug('test spec:', file);
+//      tests.push(file);
+//    }
+//  }
+//}
+
 requirejs.config({
   // Karma serves files from '/base'.
   baseUrl: '/base',
 
   packages: [
-    { name: '../../atlas/lib', location: 'atlas/lib'},
-    { name: '../../atlas/assets', location: 'atlas/assets'}, // Only need this for testing
-    { name: '../../atlas', location: 'atlas/src'},
-    { name: '../../atlas-cesium/cesium', location: 'atlas-cesium/lib/cesium'},
-    { name: '../../atlas-cesium/lib', location: 'atlas-cesium/lib'},
-    { name: '../../atlas-cesium', location: 'atlas-cesium/src'}
+    { name: 'atlas/lib', location: 'atlas/lib'},
+    { name: 'atlas/assets', location: 'atlas/assets'}, // Only need this for testing
+    { name: 'atlas', location: 'atlas/src'}
   ],
 
   // Ask requirejs to load these files.
