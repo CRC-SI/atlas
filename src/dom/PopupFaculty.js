2ea905e8ae83888ad8dba1a0b3185fe77884cb82
Brendan Studds <brendan.studds@gmail.com>
2014-05-09 17:05:27 +1000

    [WIP] Wrote some tests for the PopupFaculty and started implementing.

--- src/dom/PopupFaculty.js
+++ src/dom/PopupFaculty.js
@@ -1,45 +1,127 @@
 define([
+  'atlas/core/ItemStore',
+  'atlas/dom/Overlay',
   'atlas/util/Class',
-  'atlas/dom/Overlay'
-], function (Class, Overlay) {
+  'atlas/util/DeveloperError',
+  'atlas/util/mixin'
+], function (ItemStore, Overlay, Class, DeveloperError, mixin) {
   /**
    * @typedef atlas.visualisation.FeaturePopupFaculty
    * @ignore
    */
-  var FeaturePopupFaculty;
+  var PopupFaculty;
 
-  // Uses on EventManager:
+  // Requirements:
+  // Needs a DOM element to associate with.
+  // Uses EventManager:
   //   On 'entity/popup/show' display the overlay
   //   On 'entity/popup/hide'
   //   On 'entity/deselect'
   //     - remove and destroy the overlay
   // Depends on Overlay
 
+  // Interface:
   // The user should be able to specify:
   //   - the entityId of the associated entity (not required for functionality)
+  //   - the CSS class of the popup container
   //   - the position of the popup as
   //     - (top || bottom || 0), (left || right || 0)
   //   - the size of the popup
 
-
   // The overlay should:
+  //   - display the given html
+  //   - have the given CSS class
+  //   - be displayed in the given location, with the given size
   //   - not have an enable checkbox
   //   - have a close button
 
-  FeaturePopupFaculty = Class.extend({
+
+  PopupFaculty = Class.extend({
 
     /**
      * The Overlay being rendered by the popup.
      * @private
-     * @type atlas.dom.Overaly
+     * @type {atlas.core.ItemStore}
      */
-    _overlay: null,
+    _overlays: null,
 
-    _init: function () {
+    /**
+     * Dependency: The EventManager the PopupFaculty should register events with.
+     * @private
+     * @type {atlas.event.EventManager}
+     */
+    _eventManager: null,
 
+    /**
+     * The DOM node that popups are attached too.
+     * @private
+     * @type {HTMLElement}
+     */
+    _domNode: null,
+
+    _init: function (args) {
+      args = mixin({}, args);
+      if (!args.parentDomNode) {
+        throw new DeveloperError('PopupFaculty requires a parent DOM node to be specified.');
+      } else if (typeof args.parentDomNode === 'string') {
+        this._domNode = document.getElementById(args.parentDomNode);
+      } else {
+        this._domNode = args.parentDomNode;
+      }
+      if (!this._domNode || !this._domNode.outerHTML) {
+        throw new Error('Error associating PopupFaculty with parent dom node "'
+            + args.parentDomNode + '"');
+      }
+
+      if (args.eventManager && args.eventManager.addEventHandler) {
+        this.bindEvents();
+      }
+    },
+
+    /**
+     * Performs necessary initialisation after PopupFaculty's dependencies have been
+     * initialised.
+     * @param args
+     */
+    setup: function (args) {
+      if (this.has('eventManager')) {
+        this.bindEvents();
+      }
+    },
+
+    // -------------------------------------------
+    // Popup management
+    // -------------------------------------------
+
+    /**
+     * Generates a new Popup and shows it. The new popup is cached so it can be re-shown
+     * if necessary.
+     * @param args
+     */
+    show: function (args) {
+      this._overlays = new ItemStore();
+
+      args = mixin({
+        cssClass: this.DEFAULT_CSS_CLASS,
+        onRemove: 'close'
+      }, args);
+      if (!args.entityId) {throw new DeveloperError('Must specify entity ID associated with popup.');};
+      if (!args.content) {throw new DeveloperError('Must content of popup.');};
+      if (!args.position) {throw new DeveloperError('Must specify position of popup.');};
+
+      overlay = new Overlay(args);
+      this.setOverlay(args.entityId, overlay);
+      return overlay;
+    },
+
+    _setOverlay: function (id, overlay) {
+      this._overlays.add(id, overlay);
     }
 
   });
 
+  PopupFaculty.DEFAULT_CSS_CLASS = 'atlas-popup';
+
+  return PopupFaculty;
+
 });
-FeaturePopupFaculty
--- test/specs/dom/PopupFacultySpec.js
+++ test/specs/dom/PopupFacultySpec.js
@@ -2,8 +2,70 @@ define([
   // Code under test
   'atlas/dom/PopupFaculty'
 ], function (PopupFaculty) {
+  var popupFaculty,
+      popup,
+      entityId = '0001',
+      html = '<p>Text</p>',
+      cssClass = 'aPopup',
+      domNode,
+      args = {
+        entityId: entityId,
+        position: {top: 0, left: 0},
+        content: html
+      };
 
   describe('A PopupFaculty', function () {
+
+    beforeEach(function () {
+      domNode = document.createElement('div');
+      domNode.id = 'popupContainer';
+      document.appendChild(domNode);
+    });
+
+    afterEach(function () {
+      popupFaculty = null;
+      popup = null;
+    });
+
+    it('should be constructed with a parent dom node', function () {
+      popupFaculty = new PopupFaculty({parentDomNode: 'popupContainer'});
+      expect(popupFaculty).not.toBeNull();
+      popupFaculty = new PopupFaculty({parentDomNode: domNode});
+      expect(popupFaculty).not.toBeNull();
+    });
+
+    xit('should require an EventManager to be constructed', function () {
+      var noArgs = function () { popupFaculty = new PopupFaculty(); },
+          noManager = function () { popupFaculty = new PopupFaculty({}); },
+          emptyManager = function () { popupFaculty = new PopupFaculty({eventManager: {}}); },
+          incorrectManager = function () { popupFaculty = new PopupFaculty({eventManager: null}); };
+
+      expect(noArgs).toThrow();
+      expect(noManager).toThrow();
+      expect(emptyManager).toThrow();
+      expect(incorrectManager).toThrow();
+    });
+
+    it('should show popups given HTML content, a top-left position, and a entity ID', function () {
+      popup = popupFaculty.show(args);
+      expect(popup.isVisible()).toBe(true);
+      expect(popup.getContent()).toEqual(html);
+      expect(popup.getPosition()).toEqual(position);
+      expect(popup.getEntityId()).toEqual(entityId);
+    });
+
+    it('should allow a CSS class for the overlay contained be set', function () {
+      args.cssClass = cssClass;
+      popup = popupFaculty.show(args);
+      expect(popup.getCssClass()).toEqual(cssClass);
+    });
+
+    it('should hide popup identify by associated entity ID', function () {
+      popup = popupFaculty.showPopup(args);
+      expect(popup.isVisible()).toBe(true);
+      popup = popupFaculty.hidePopup(entityId)
+      expect(popup.getDom()).toBeNull();
+    });
 
   });
 });
