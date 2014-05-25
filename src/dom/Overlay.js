4e334e90600361de1664a94a03f63bb7585e9491
Aram Kocharyan <akarmenia@gmail.com>
2014-05-25 18:45:18 +1000

    Updated the overlay for colour projections to work with the new Overlay properties. Used deep mixin for overlay properties to reduce code.

--- src/dom/Overlay.js
+++ src/dom/Overlay.js
@@ -1,7 +1,7 @@
 define([
   'atlas/util/Class',
-  'atlas/util/mixin'
-], function (Class, mixin) {
+  'atlas/lib/utility/Setter'
+], function (Class, Setter) {
 
   /**
    * @classdesc An Overlay can be used to place panels overlaying the Atlas
@@ -84,13 +84,7 @@ define([
      * @ignore
      */
     _init: function (args) {
-      ['top', 'left', 'right', 'bottom'].forEach(function (p) {
-        args.position[p] === null && delete args.position[p];
-      });
-      args.dimensions.width === null && delete args.dimensions.width;
-      args.dimensions.height === null && delete args.dimensions.height;
-
-      args = mixin({
+      args = Setter.mixin({
         parent: document,
         cssClass: '',
         title: '',
@@ -98,10 +92,9 @@ define([
         dimensions: {width: 0, height: 0},
         content: ''
       }, args);
-      if (typeof args.parent === 'string') { args.parent = document.getElementById(parent); }
-
-
-
+      if (typeof args.parent === 'string') {
+        args.parent = document.getElementById(parent);
+      }
       this._id = args.id;
       this._parent = args.parent;
       this._cssClass = args.cssClass;
