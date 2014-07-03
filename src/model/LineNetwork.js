cb177d5c92218926356c3fa94314bb6441cf17af
Brendan Studds <brendan.studds@gmail.com>
2014-07-03 13:29:15 +1000

    Added basic construction of LineNetwork.

--- src/model/LineNetwork.js
+++ src/model/LineNetwork.js
@@ -1,15 +1,59 @@
 define([
   'atlas/model/GeoEntity',
-  'atlas/lib/utility/Log'
-], function (GeoEntity, Log) {
+  'atlas/lib/utility/Log',
+  'atlas/lib/utility/Setter'
+], function (GeoEntity, Log, Setter) {
   /**
    * @typedef atlas.model.LineNetwork
    * @ignore
    */
   var LineNetwork;
 
+  /**
+   * @classdesc A LineNetwork represents a 2D graph of lines.
+   * @class atlas.model.LineNetwork
+   * @extends atlas.model.GeoEntity
+   */
   LineNetwork = GeoEntity.extend({
+    /**
+     * This is an array of objects containing the data required to construct and render the lines
+     * making up the LineNetwork. The geometry of the line is determined by
+     * <code>lineData.vertexIds</code>. The <code>vertexId</code> refers to the index of the
+     * GeoPoints constructing the line in <code>vertexData</code>.
+     * @type {Array.<atlas.model.LineNetwork#LineData>}
+     * @private
+     */
+    _lineData: null,
 
+    /**
+     * This is an array of all the vertices that are present in the LineNetwork.
+     * @type {Array.<atlas.model.GeoPoint>}
+     * @private
+     */
+    _vertexData: null,
+
+    _init: function (args) {
+      this._super(args);
+
+      args = Setter.mixin({
+        vertexData: [],
+        lineData: []
+      }, args);
+
+      this._vertexData = args.vertexData;
+      this._lineData = args.lineData;
+    },
+
+    // -------------------------------------------
+    // Getters and Setters
+    // -------------------------------------------
+    getLines: function () {
+      return this._lineData;
+    },
+
+    getVertices: function () {
+      return this._vertexData;
+    }
   });
 
   return LineNetwork;
--- test/specs/model/LineNetworkSpec.js
+++ test/specs/model/LineNetworkSpec.js
@@ -1,15 +1,47 @@
 define([
+  'atlas/model/GeoPoint',
   // Code under test
   'atlas/model/LineNetwork'
-], function (LineNetwork) {
+], function (GeoPoint, LineNetwork) {
   var lineNw,
-      id = 'lineNw';
+      id = 'lineNw',
+      args,
+      vertices = [
+        new GeoPoint(0, 0),
+        new GeoPoint(0, 1),
+        new GeoPoint(1, 1),
+        new GeoPoint(1, 0)
+      ],
+      lines = [
+        {vertexIds: [0, 2, 3]},
+        {vertexIds: [0, 1]}
+      ];
 
   describe('A LineNetwork', function () {
+    afterEach(function () {
+      lineNw = null;
+    });
+
     it('should be constructable with just an ID', function () {
       lineNw = new LineNetwork(id);
       expect(lineNw).not.toBeNull();
       expect(lineNw.getId()).toEqual(id);
-    })
+
+      lineNw = new LineNetwork({id: id});
+      expect(lineNw).not.toBeNull();
+      expect(lineNw.getId()).toEqual(id);
+    });
+
+    it('should construct lines if given appropriate structures using GeoPoints', function () {
+      args = {
+        id: id,
+        vertexData: vertices,
+        lineData: lines
+      };
+      lineNw = new LineNetwork(args);
+      expect(lineNw).not.toBeNull();
+      expect(lineNw.getVertices()).toEqual(vertices);
+      expect(lineNw.getLines()).toEqual(lines);
+    });
   });
 });
