3bce99cd1bc3af8af5087e45c68b834323d5f7c5
Brendan Studds <brendan.studds@gmail.com>
2014-07-24 16:46:13 +1000

    Added LineDrawModule to manage drawing lines
    The LineDrawModule controls drawing 2D lines and hooks into the
    EditManager of Atlas. Currently the module is a copy of the existing
    DrawModule which was created with the intent of drawing Polygons. These
    two modules should be refactored into an abstract DrawModule, as well as
    a PolygonDrawnModule and a LineDrawModule.

---
+++ test/specs/edit/LineDrawModuleSpec.js
@@ -0,0 +1,35 @@
+define([
+  'atlas/edit/EditManager',
+  'atlas/entity/EntityManager',
+  'atlas/events/EventManager',
+  'atlas/model/Feature',
+  'atlas/render/RenderManager',
+  // Code under test
+  'atlas/edit/LineDrawModule'
+], function (EditManager, EntityManager, EventManager, Feature, RenderManager, LineDrawModule) {
+  var drawModule,
+      managers;
+
+  describe('A LineDrawModule', function () {
+    beforeEach(function () {
+      managers = {};
+      // Managers store themselves on managers
+      new EditManager(managers);
+      new EventManager(managers);
+      new EntityManager(managers);
+      new RenderManager(managers);
+      drawModule = new LineDrawModule(managers)
+    });
+
+    afterEach(function () {
+      drawModule = null;
+    });
+
+    it('should construct a new Feature when setup', function () {
+      drawModule._setup();
+      expect(drawModule._feature).not.toBeNull();
+      expect(drawModule._feature._displayMode).toBe(Feature.DisplayMode.LINE);
+    });
+  });
+});
+
