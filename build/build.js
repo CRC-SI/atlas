e3ae9e1f044f188ce254ffb3cd460a754478be8e
Aram Kocharyan <akarmenia@gmail.com>
2014-08-05 17:33:09 +1000

    Added build path for utility.

--- build/build.js
+++ build/build.js
@@ -3,7 +3,8 @@
   baseUrl: '../src',
   paths: {
     'atlas': '',
-    'atlas/lib': '../lib'
+    'atlas/lib': '../lib',
+    'atlas/lib/utility': '../lib/utility/src'
   },
   name: 'main',
   out: '../dist/atlas.min.js',
