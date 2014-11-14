58f8d964571293097a972c5ebff3705b30a4fed4
Aram Kocharyan <akarmenia@gmail.com>
2014-11-14 12:03:30 +1100

    Fixed issues with package.js.

--- package.js
+++ package.js
@@ -2,12 +2,13 @@
 Package.describe({
   name: 'urbanetic:atlas',
   version: '0.7.0-beta',
-  summary: 'An API for a 3D Volumetric rendering library.'
+  summary: 'An API for a 3D Volumetric rendering library.',
+  git: 'https://bitbucket.org/mutopia/atlas.git'
 });
 
 Package.onUse(function (api) {
   api.versionsFrom('METEOR@0.9.0');
-  api.use('aramk:requirejs', ['client', 'server']);
+  api.use('aramk:requirejs@2.1.15', ['client', 'server']);
   api.addFiles(['dist/atlas.min.js'], ['client', 'server']);
   api.addFiles(['dist/resources/atlas.min.css'], 'client');
 });
