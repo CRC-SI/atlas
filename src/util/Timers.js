5a03f8a8a97eb5d8bdfd0b66f8a67456ecd78949
Aram Kocharyan <akarmenia@gmail.com>
2014-11-28 20:52:30 +1100

    Added timer utility with a waitUtil() method for delaying execution until a predicate become truthy.

---
+++ src/util/Timers.js
@@ -0,0 +1,53 @@
+define([
+  'atlas/lib/utility/Setter',
+  'atlas/lib/Q'
+], function(Setter, Q) {
+  /**
+   * A set of timer utility methods.
+   */
+  return {
+
+    /**
+     * Waits until the given predicate evaluates to a truthy value.
+     * @param  {Function} predicate
+     * @param  {Object} args
+     * @param  {Number} args.timeout - The time in milliseconds to continue waiting. After this
+     * period, the promise is rejected.
+     * @param  {Number} args.freq - The frequency in milliseconds to check the predicate.
+     * @returns {Q.Deferred} A deferred promise which is resolved when the given predicate evaluates
+     * to a truthy value.
+     */
+    waitUntil: function(predicate, args) {
+      var df = Q.defer();
+      if (predicate()) {
+        df.resolve();
+      } else {
+        args = Setter.merge({
+          timeout: 60000,
+          freq: 200
+        }, args);
+        var timeout = args.timeout;
+        var freq = args.freq;
+        if (freq > timeout) {
+          throw new Error('Frequency must be less than timeout period.');
+        }
+        var totalTime = 0;
+        var handle = setInterval(function() {
+          var isDone = predicate();
+          if (isDone) {
+            df.resolve();
+          }
+          if (totalTime >= timeout) {
+            clearInterval(handle);
+            if (!isDone) {
+              df.reject();
+            }
+          }
+          totalTime += freq;
+        }, freq);
+      }
+      return df;
+    }
+
+  }
+});
