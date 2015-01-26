c94dc25c6f76186be92b166ee590b403e5928019
Aram Kocharyan <akarmenia@gmail.com>
2015-01-26 11:01:48 +1100

    Version bump for meteor.

--- package.js
+++ package.js
@@ -1,14 +1,14 @@
 // Meteor package definition.
 Package.describe({
   name: 'urbanetic:atlas',
-  version: '0.7.0-beta',
+  version: '0.7.0_1',
   summary: 'An API for a 3D Volumetric rendering library.',
   git: 'https://bitbucket.org/mutopia/atlas.git'
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('METEOR@0.9.0');
-  api.use('aramk:requirejs@2.1.15', ['client', 'server']);
+  api.use('aramk:requirejs@2.1.15_1', ['client', 'server']);
   api.addFiles(['dist/atlas.min.js'], ['client', 'server']);
   api.addFiles(['dist/resources/atlas.min.css'], 'client');
 });
