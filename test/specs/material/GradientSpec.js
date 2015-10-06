45e49cb1ab97627a6a678e6bcde46242e7c1c426
Aram Kocharyan <akarmenia@gmail.com>
2015-10-07 02:08:44 +1100

    Add expect() to GradientSpec.

--- test/specs/material/GradientSpec.js
+++ test/specs/material/GradientSpec.js
@@ -4,26 +4,46 @@ define([
   'atlas/material/Color',
   'underscore'
 ], function(Log, EntityManager, Color, _) {
-  describe ('A Gradient', function() {
+  describe('A Gradient', function() {
 
-    describe ('can be constructed', function() {
+    describe('can be constructed', function() {
 
-      it ('from C3ML', function() {
+      it('from C3ML', function() {
         var c3ml = {
           id: 'foo',
           type: 'polygon',
           coordinates: [],
-          fillMaterial: {type:'Gradient', colors: [
-            {color: [125, 233, 80, 255], pivot: 0.0},
-            {color: [233, 233, 80, 255], pivot: 0.5},
-            {color: [233, 120, 80, 255], pivot: 1.0}
-          ]}
+          fillMaterial: {
+            type: 'Gradient',
+            colors: [{
+              color: [125, 233, 80, 255],
+              pivot: 0.0
+            }, {
+              color: [233, 233, 80, 255],
+              pivot: 0.5
+            }, {
+              color: [233, 120, 80, 255],
+              pivot: 1.0
+            }]
+          }
         };
         var entityManager = new EntityManager({});
         entityManager.bulkCreate([c3ml]);
         var entity = entityManager.getById('foo');
         var gradient = entity.getStyle().getFillMaterial();
-        console.log('gradient', JSON.stringify(gradient.toJson()));
+        expect(gradient.toJson()).toEqual({
+            type: 'Gradient',
+            colors: [{
+              pivot: 0,
+              color: 'rgba(0, 0, 0, 1)'
+            }, {
+              pivot: 0.5,
+              color: 'rgba(0, 0, 0, 1)'
+            }, {
+              pivot: 1,
+              color: 'rgba(0, 0, 0, 1)'
+            }]
+          })
       });
 
     });
